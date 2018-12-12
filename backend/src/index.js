fece3f559a943c013b6609a0f2748729f0c57bc9
thatjp <jpharris.contact@gmail.com>
2018-12-12 10:58:01 -0500

    things

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,18 @@
 // let's go!
+const cookieParser = require('cookie-parser');
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.express.use(cookieParser());
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  },
+}, (deets) => {
+  console.log(`server is now running on port http://localhost:${deets.port}`);
+});
