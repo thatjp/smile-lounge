080dc00c59ae8412fad70d43ce0362cc0bbb23dd
thatjp <jpharris.contact@gmail.com>
2018-12-12 10:58:01 -0500

    things

---
+++ src/db.js
@@ -0,0 +1,11 @@
+// allows us to affect the remote graphql server with javaxript
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret:  process.env.PRISMA_SECRET,
+  debug: false
+})
+
+module.exports = db
