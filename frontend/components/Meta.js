798c98a0094615d30a2986c46c87bb60268e6ad4
thatjp <jpharris.contact@gmail.com>
2018-12-11 21:27:47 -0500

    init commit

---
+++ frontend/components/Meta.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width" />
+    <meta charSet="utf-8" />
+    {/* <link rel="shortcut icon" href="/static/favicon.png" /> */}
+    <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+    <title>Smile Lounge</title>
+  </Head>
+);
+
+export default Meta;
