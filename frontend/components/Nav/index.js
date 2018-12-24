f181f6569e61a39cf00c33e1dcc19a9514c8577d
thatjp <jpharris.contact@gmail.com>
2018-12-23 19:09:15 -0500

    + created user query

--- frontend/components/Nav/index.js
+++ frontend/components/Nav/index.js
@@ -1,8 +1,19 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
 import Link from 'next/link';
 import NavStyle from './style';
+import User from '../User';
 
 const Nav = () => (
   <NavStyle>
+    <User>
+      {({ data: { me } }) => {
+        if (me) {
+          return <p>{me.name}</p>;
+        }
+        return null;
+      }}
+    </User>
     <Link href="/cart">
       <a>Cart</a>
     </Link>
