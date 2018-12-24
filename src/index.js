8392cfa48fb590c8468ded9563802cbc5ae9f347
thatjp <jpharris.contact@gmail.com>
2018-12-23 19:09:15 -0500

    + created user query

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-  const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
+const jwt = require('jsonwebtoken');
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
 server.express.use(cookieParser());
+
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start({
   cors: {
