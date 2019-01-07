09a3ca9d218af009c3979c36ce7e327acfbc9c1d
thatjp <jpharris.contact@gmail.com>
2019-01-07 17:33:23 -0500

    about page mobile padding edit

--- frontend/components/About/styles.js
+++ frontend/components/About/styles.js
@@ -4,7 +4,9 @@ const AboutPageStyles = styled.div`
   display: flex;
   justify-content: center;
   padding: 30px 300px;
-  
+  @media (max-width: 700px) {
+    padding: 20px 30px;
+  }
 `;
 
 export default AboutPageStyles;
